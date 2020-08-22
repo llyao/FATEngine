e73a136d9d85916723c1ced741100319c5bbdfb0
yaolinglong <yaolinglong@ztgame.com>
2020-08-22 16:29:33 +0800

    * IServiceLocator.cs

---
+++ Client/FatBoll/Assets/Script/Kernel/IServiceLocator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fat.Kernel
+{
+    public interface IServiceLocator
+    {
+        void Register<InterType, ImpleType>() where ImpleType : InterType;
+
+
+
+    }
+}
+
